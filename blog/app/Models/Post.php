1b83e60eeb074ad5384e4858cefa9767988e15a3
hector <hfelicesg@fp.insjoaquimmir.cat>
2023-12-13 12:58:45 +0100

    proyecto blog

---
+++ blog/app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // Relacion 1-n inversa
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    // Relacion n-m
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+
+    // Relacion 1-1 polimorfica
+
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
+}
